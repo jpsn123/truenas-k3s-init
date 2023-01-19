6b88fbb65dab73e69cdd091f0b1c99b204c3673c
yiqin.li <yiqin.li@jutze.com.cn>
2023-01-20 02:54:41 +0800

    hdparm replace smartctl, fix weird reads.

--- disk-mon.sh
+++ disk-mon.sh
@@ -12,8 +12,8 @@ do
     for i in $dsk
     do
         echo -n "/dev/$i : " 
-        s=`smartctl -i -n standby /dev/$i|grep "mode"|awk '{print $4}' ` # 正儿八经的Linux系统可以用这个，也可以用下面那个
-#        s=` hdparm -C /dev/$i|grep "drive state is:" |awk '{print $4}'|awk -F "/" '{print $1}'` 
+#        s=`smartctl -i -n standby /dev/$i|grep "mode"|awk '{print $4}' ` # 正儿八经的Linux系统可以用这个，也可以用下面那个
+        s=`hdparm -C /dev/$i|grep "drive state is:" |awk '{print $4}'|awk -F "/" '{print $1}'` 
          #群晖必须用这一条，因为使用smartd查看HDD的状态可能会提示该设备未打开smart支持，但是实际上是能够在DSM的存储空间管理员中看到相应磁盘的smart信息的，应该是DSM自带的smartctl工具有问题
         if [[ -f "$path/Disks/$i.status" ]];then
             st=`cat $path/Disks/$i.status`
