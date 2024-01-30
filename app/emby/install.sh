87581e93f9591f6d3a989e338ad5d200bbfacfa5
yiqin.li <yiqin.li@jutze.com.cn>
2024-01-30 12:59:18 +0800

    emby: change chart template

--- app/emby/install.sh
+++ app/emby/install.sh
@@ -15,8 +15,8 @@ sed -i "s/example.com/${DOMAIN}/g" values-emby.yaml
 # install emby
 #####################################
 echo -e "\033[42;30m install emby \n\033[0m"
-helm repo add k8s-at-home https://k8s-at-home.com/charts/
-[ -d temp/emby ] || helm pull k8s-at-home/emby --untar --untardir temp
+helm repo add bjw-s https://bjw-s.github.io/helm-charts
+[ -d temp/emby ] || helm pull bjw-s/app-template --untar --untardir temp
 METHOD=install
 [ `app_is_exist $NS emby` == true ] && METHOD=upgrade
 helm $METHOD -n $NS emby temp/emby -f values-emby.yaml
