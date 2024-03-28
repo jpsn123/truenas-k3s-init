0676485657bc3962fbe29630d860c63cd3009d00
yiqin.li <yiqin.li@jutze.com.cn>
2024-03-28 10:47:07 +0800

    remove disk sleep helper script

--- start-disk-sleep.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DISK1=`ls -l /dev/disk/by-id/ata-ST2000DM006-2DM164_W4Z4JD2S | sed -e 's#.*\(sd.\)#\1#'`
-DISK2=`ls -l /dev/disk/by-id/ata-ST3000DM001-1ER166_W502Y599 | sed -e 's#.*\(sd.\)#\1#'`
-nohup bash /root/truenas-k3s-init/spindown_timer.sh -q -t 600 -p 600 -i $DISK1 -i $DISK2 &>/dev/null &
-nohup bash /root/truenas-k3s-init/spindown_timer.sh -q -t 1800 -p 600 -m -i $DISK1 -i $DISK2 &>/dev/null &
