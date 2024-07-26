161e1f339b3129d7c7f4f1a82206b58acbe5c9d8
yiqin.li <yiqin.li@jutze.com.cn>
2024-07-26 22:47:02 +0800

    ldap fix

--- app/ldap/restart-ldap.sh
+++ app/ldap/restart-ldap.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-PODS=`kubectl get pod -n ldap -l app=ldap-openldap -o jsonpath='{.items[*].metadata.name}'`
+PODS=`kubectl get pod -n ldap -l app.kubernetes.io/instance=ldap -o jsonpath='{.items[*].metadata.name}'`
 for pod in ${PODS[*]}
 do
   kubectl delete -n ldap pod $pod
   for ((i=0;i<100;i++))
   do
-      RES=`kubectl -n ldap rollout status statefulset ldap-openldap -w=false 2>/dev/null|grep -E 'successfully|complete' || true`
+      RES=`kubectl -n ldap rollout status statefulset ldap -w=false 2>/dev/null|grep -E 'successfully|complete' || true`
       if [ -n "$RES" ]; then
-          echo -e "resource ldap-openldap is ready!"
+          echo -e "resource ldap is ready!"
           break
       fi
-      echo -e "waiting ldap-openldap be ready..."
+      echo -e "waiting ldap be ready..."
       sleep 5
   done
 done
