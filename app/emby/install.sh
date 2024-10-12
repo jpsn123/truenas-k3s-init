c261930168b86c0236e9d9de802092769856debe
yiqin.li <yiqin.li@jutze.com.cn>
2024-10-12 16:39:00 +0800

    remove alist, jellyfin, losslesscut,odoo apps

--- app/emby/install.sh
+++ app/emby/install.sh
@@ -25,12 +25,3 @@ METHOD=install
 [ `app_is_exist $NS $APP_NAME` == true ] && METHOD=upgrade
 helm $METHOD -n $NS $APP_NAME temp/app-template -f values-emby.yaml
 k8s_wait $NS deployment $APP_NAME 100
-
-# install MetaTube, copy MetaTube.dll to plugins dir
-#####################################
-echo -e "\033[42;30m install MetaTube, copy MetaTube.dll to plugins dir \n\033[0m"
-kubectl apply -n $NS -f plugin-deploy.yaml
-POD_NAME=`kubectl get pod -n $NS -l app.kubernetes.io/name=emby -o jsonpath="{.items[0].metadata.name}"`
-kubectl cp MetaTube.dll $NS/$POD_NAME:/config/plugins -c emby
-kubectl cp MetaTube.xml $NS/$POD_NAME:/config/plugins/configurations -c emby
-kubectl delete -n $NS pod $POD_NAME
