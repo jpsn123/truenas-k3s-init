cb821ddf3e36e4da32df23ebc698f88d64b9c605
yiqin.li <yiqin.li@jutze.com.cn>
2025-01-11 14:47:08 +0800

    add minio

---
+++ app/minio/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd $(dirname $0)
+source ../../common.sh
+source ../../parameter.sh
+NS=minio
+APP_NAME=minio
+
+# initial
+#####################################
+log_info "initial"
+[ -d temp ] || mkdir temp
+kubectl create namespace $NS 2>/dev/null
+sed -i "s/example.com/${DOMAIN}/g" values.yaml
+sed -i "s/sc-example/${STORAGE_CLASS_NAME}/g" values.yaml
+log_reminder "please input password for minio."
+read -p "password:"
+
+# install
+#####################################
+log_info "install $APP_NAME"
+helm repo add bitnami https://charts.bitnami.com/bitnami
+[ -d temp/minio ] || helm pull bitnami/minio --untar --untardir temp
+helm upgrade --install -n $NS $APP_NAME temp/minio -f values.yaml \
+    --set auth.rootPassword=$REPLY
+
+## done
+log_trace "init success!!!"
