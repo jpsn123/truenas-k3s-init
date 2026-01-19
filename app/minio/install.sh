bed64dab359905461393ac019b25355c81581c5a
yiqin.li <yiqin.li@jutze.com.cn>
2026-01-19 17:01:20 +0800

    minio: fix

--- app/minio/install.sh
+++ app/minio/install.sh
@@ -21,7 +21,7 @@ MINIO_PW=$(echo -n "$REPLY@$NS@minio" | sha1sum | awk '{print $1}' | base64 | he
 #####################################
 log_info "install $APP_NAME"
 [ -d temp/minio ] || (helm pull oci://registry-1.docker.io/bitnamicharts/minio --untar --untardir temp --version=16.0.10)
-helm upgrade --install -n $NS $APP_NAME temp/minio -f temp/values.yaml \
+helm upgrade --install -n $NS $APP_NAME temp/minio -f temp/values-minio.yaml \
     --set auth.rootPassword=$MINIO_PW
 
 ## done
